7ee55da30dcf7fe1c0987995adfdd8b34e15531b
= <=>
2023-10-30 05:33:49 +0530

    added routes

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print ("hello!")
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
