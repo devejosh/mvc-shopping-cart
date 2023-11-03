2c66690af0f202a5f82f1eff78b0751b844e3b2c
= <=>
2023-11-03 07:33:42 +0530

    added a route to cart templete

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
 
 
 if __name__ == '__main__':
