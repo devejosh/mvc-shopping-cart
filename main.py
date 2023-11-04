c7bc91ab1fe7f3dcf18a2dab13d0c99093fe9490
= <=>
2023-11-04 06:43:33 +0530

    added some code to process json file to load product details

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify, json
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    try:
+        with open('data/products.json', 'r') as products:
+            data = json.load(products)
+        return render_template('index.html', data=data)
+    except FileNotFoundError :
+        return render_template('404.html')
+    
 
 
 @app.route('/contact')
@@ -15,7 +24,10 @@ def contact():
 
 @app.route('/cart')
 def cart():
+
     return render_template('cart.html')
+
+
 
 
 if __name__ == '__main__':
