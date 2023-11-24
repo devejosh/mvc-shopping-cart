fb2f6f8482fa4f5394f55be6af0e1407a47a6c74
= <=>
2023-11-24 12:19:22 +0530

    added an external config file to centralize configs

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+#using absolute paths to address issues with file access
+class Config:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    
+    #Paths
+    PRODUCTS_FILE = os.path.join(BASE_DIR, 'data/products.json')
+    WRITE_TO_CART = os.path.join(BASE_DIR, 'data/cart.json')
+    EMPTY_CART = 'components/emptycart.html'
+    ERROR_404 = 'components/404.html'
+    
