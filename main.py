42ab10c443b344e592e8cffd34545aa8ec0321b2
= <=>
2024-05-12 22:49:42 +0530

    fixed the addtocart logic. made changes to routes to fix the app

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #default imports
 from flask import Flask, render_template, request, redirect, url_for, json
+import traceback
 
 #custom imports
 from config import Config
 from exception_handler import exceptions
 from app.models.models import shoppingcart
+from utils import helper
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +15,7 @@ app.config.from_object(Config)
 # Instantiate an empty shopping cart at the start
 my_cart = shoppingcart(app.config)
 checkexceptions = exceptions()
+
 
 
 # Custom error page for 404 (page not found errors)
@@ -51,6 +54,7 @@ def cart():
                     total = my_cart.calculate_total() 
                     return render_template('cart.html', data=cartdata,  finalttl = total)
         except:
+                traceback.print_exc()
                 return render_template(app.config['ERROR_404'])
         
 
@@ -61,10 +65,13 @@ def addtocart():
             if request.method == 'POST':
                 
                 #Check data for exceptions
-                error, price_type, quantity_type = checkexceptions.checkdata(request.form.get('product_name'), request.form.get('price'), request.form.get('quantity'), request.form.get('product_image'))
+                error = checkexceptions.checkdata(request.form.get('product_name'), request.form.get('price'), request.form.get('quantity'), request.form.get('product_image'))
+              
+              
               # check if the checkdata method retuned an error
                 if error:
-                   return render_template(app.config['INVALID_INPUT'], data=error, price_type=price_type, quantity_type=quantity_type)
+                   return render_template(app.config['INVALID_INPUT'], data=error)
+                
                 else:
                     if error is None:
                         print("We are adding some data to the cart now")
@@ -73,15 +80,24 @@ def addtocart():
                         price = request.form.get('price')
                         quantity = request.form.get('quantity')
                         product_image = request.form.get('product_image')
+                        print ("calling the cart function")
                         
                         #Call the add product method from the shopping cart class to add the product to cart.
+                        #Instaciate the helper class to make sure quanitity and price are integers and not strings. This helps when doing calculations. 
+                        
+                        helper_instance = helper()
+                        quantity = helper_instance.safe_int(quantity)
+                        price = helper_instance.safe_int(price)
+
                         my_cart.add_product(product_name, quantity, price, product_image)
 
-                        return(render_template('index.html'))
+                        #retuning to the index page after adding items to cart
+                        print ("Item added to the cart, returning to the index page")
+                        return redirect(url_for('home'))
             else:
                  print("Issue with the code. We are checking.")
         except:
-             return render_template(app.config['ERROR_404'], data="Exception was generated here")
+             return render_template(app.config['ERROR_404'], data=traceback.print_exc())
 
 
 @app.route('/remove-from-cart', methods=['POST'])
