87e1e4388a4ce91e1f04be43bf4c5e88dc222d50
= <=>
2024-05-20 06:32:25 +0530

    minor changes to the config file

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 #using absolute paths to address issues with file access
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    
-    #Paths
+
+    #file Paths
     PRODUCTS_FILE = os.path.join(BASE_DIR, 'data/products.json')
     WRITE_TO_CART = os.path.join(BASE_DIR, 'data/cart.json')
     EMPTY_CART = 'components/emptycart.html'
