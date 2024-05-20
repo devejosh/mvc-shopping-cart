6e0c410cd428228dfab56d7c3d48e930bd70d55d
= <=>
2024-05-20 06:33:18 +0530

    breaking changes. Working on implementing the new project structure

--- main.py
+++ main.py
@@ -1,126 +1,51 @@
 #default imports
-from flask import Flask, render_template, request, redirect, url_for, json
-import traceback
+from flask import Flask, render_template, request
 import logging
 
-#custom imports
-from config import Config
-from exception_handler import exceptions
-from app.models.models import shoppingcart
-from utils import helper
+
+#Importing all controllers
+from app.controllers.product import products_controller
+from app.controllers.routes import route_controller
+from app.controllers.cart import cart_controller
+
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
 
 # Setup logging
 logging.basicConfig(filename='error.log', level=logging.ERROR)
 
-# Instantiate an empty shopping cart at the start
-my_cart = shoppingcart(app.config)
-checkexceptions = exceptions()
-
-
-
-# Custom error page for 404 (page not found errors)
-@app.errorhandler(404)
-def pagenotfound(error):
-    app.logger.error(f'Page not found: {request.path}, error: {error}')
-    return render_template(app.config['ERROR_404']), 404
-
 
 @app.route('/')
 def home():
-    try:
-        with open(app.config['PRODUCTS_FILE'], 'r') as products:
-            data = json.load(products)
-        return render_template('index.html', data=data)
-    except FileNotFoundError:
-        return render_template(app.config('ERROR_404'))
+    data = products_controller()
+    return data.get_products()
+    
 
 
 @app.route('/contact')
 def contact():
-    try:
-        return render_template('contact.html')
-    except:
-        return render_template("404.html")
-
+    route = route_controller()
+    route.contact()
 
 
 @app.route('/cart')
 def cart(): 
-        try:
-            #check if my_cart is empty. 
-            if my_cart.cart_empty() == True:
-                return render_template(app.config['EMPTY_CART'])
-            else:
-                    cartdata = my_cart.items
-                    total = my_cart.calculate_total() 
-                    return render_template('cart.html', data=cartdata,  finalttl = total)
-        except:
-                traceback.print_exc()
-                return render_template(app.config['ERROR_404'])
-        
+    cart = cart_controller()
+    cart.view_cart()
 
 
-@app.route('/addtocart', methods=['GET', 'POST'])
+@app.route('/add-to-cart' , methods=['POST'])
 def addtocart():
-        try:  
-            if request.method == 'POST':
-                
-                #Check data for exceptions
-                error = checkexceptions.checkdata(request.form.get('product_name'), request.form.get('price'), request.form.get('quantity'), request.form.get('product_image'))
-              
-              
-              # check if the checkdata method retuned an error
-                if error:
-                   return render_template(app.config['INVALID_INPUT'], data=error)
-                
-                else:
-                    if error is None:
-                        print("We are adding some data to the cart now")
-                        #Store data from the form in variables
-                        product_name = request.form.get('product_name')
-                        price = request.form.get('price')
-                        quantity = request.form.get('quantity')
-                        product_image = request.form.get('product_image')
-                        print ("calling the cart function")
-                        
-                        #Call the add product method from the shopping cart class to add the product to cart.
-                        #Instaciate the helper class to make sure quanitity and price are integers and not strings. This helps when doing calculations. 
-                        
-                        helper_instance = helper()
-                        quantity = helper_instance.safe_int(quantity)
-                        price = helper_instance.safe_int(price)
-
-                        my_cart.add_product(product_name, quantity, price, product_image)
-
-                        #retuning to the index page after adding items to cart
-                        print ("Item added to the cart, returning to the index page")
-                        return redirect(url_for('home'))
-            else:
-                 print("Issue with the code. We are checking.")
-        except:
-             return render_template(app.config['ERROR_404'], data=traceback.print_exc())
+        cart = cart_controller()
+        cart.add_to_cart()
 
 
-@app.route('/remove-from-cart', methods=['POST'])
+@app.route('/remove-from-cart')
 def removefromcart():
-    if request.method == 'POST':
-        
-        #process the request and remove the data from the cart array based on the prduct name
-        try:
-            product = request.form.get('product')
-            my_cart.remove_product(product)
-        except Exception as e:
-            print(f"Error removing product from the cart : {str(e)}")
-    return redirect(url_for('cart'))
-
-
-@app.errorhandler(Exception)
-def handle_exception(error):
-    app.logger.error(f'Unhandled exception: {error}')
-    return render_template('error.html'), 500
+     cart = cart_controller()
+     cart.remove_from_cart()
+    
 
 
 if __name__ == '__main__':
